83def9e7eb5c3438eea6a437b5efb7756280be15
Landeremmanson <solomonemmanson491@gmail.com>
2024-11-06 10:02:39 +0100

    first commit

---
+++ week1/lesson1.py
@@ -0,0 +1,10 @@
+print("-------")
+print("Hello world")
+print("-------")
+print("Solomon Lander")
+print("my is Solomon Lander")
+
+Firstname = input("Enter First Name >> ")
+secondname = input("Enter second Name >> ")
+
+print(f"My name is {Firstname} {secondname}")
