d0570caf0ecce5eedff5a52a7d8c8f4565e55552
Landeremmanson <solomonemmanson491@gmail.com>
2024-11-10 00:01:16 +0100

    hhhh

--- week1/lesson1.py
+++ week1/lesson1.py
@@ -1,15 +1,30 @@
-print("-------")
-print("Hello world")
-print("-------")
-print("Solomon Lander")
-print("my is Solomon Lander")
+#series of words"""""""
+serie_name = input('verb: ')
+serie_name = input('noun: ')
+serie_name = input('verb: ')
+serie_name = input('exclamation: ')
+serie_name = input('adjective: ')
+serie_name = input('animal: ')
+serie_name = input('verb: ')
+print(serie_name + serie_name)
 
-# converting string to integer or whole number
-firstnumber = int(input("Enter First Name >> "))
-secondnumber = int(input("Enter second Name >> "))
+# My story line""" 
+print()
+print(' My story line: ')
+print()
+print(' The other day, after returning from church. I decided to study my book and prepare for my next week course work, i head my siblings crying and some were very happy and i asked, what happen, they said that my dog gave birth, i was like wow oh. out of excitement i give my friend one of the puppy. ')
 
-# print(f"My name is {Firstname} {secondname}")
-
-total = firstnumber + secondnumber
-print(total)
-firstnumber = int(input("Hello World") )
+# series of words"""""
+print()
+serie_name = input('verb: ')
+serie_name = input('verb: ')
+serie_name = input('adjective: ')
+serie_name = input('noun: ')
+serie_name = input('exclamation: ')
+print(serie_name + serie_name)
+print()
+# frequently asking questions / My social life story line""""".
+print('frequently asking questions:')
+print()
+print('  Most time people find it very difficult to n ask questions and they think asking question in public or during conversation with someone will bring shame and people will make fun of them. During our institute class last week, I have a question to ask but I was very shied to say it out, so when my institute instructor notes, she called and said, please ask your question at that moment I did know what to do  because all the class members to hear from me, I pray to Heavenly Father for strength and confidence. And with the help of the Holy Ghost I was able to asked a meaningful and brilliant question and I received a unique answers from class members.')
+print()
