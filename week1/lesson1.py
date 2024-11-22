3d12699bbf9ef206c96cb08cbe8ae296b31b61bd
Landeremmanson <solomonemmanson491@gmail.com>
2024-11-22 20:59:28 +0100

    ...

--- week1/lesson1.py
+++ week1/lesson1.py
@@ -1,15 +1,23 @@
-# the creativity used was to change all variables to upprcase so that they display as capital lettters when the prgram is run 
+# '''the creativity used was to change all variables to upprcase so that they display as capital lettters when the prgram is run''''''
+
+print("Hello user! Welcome to my story.")
 
 #series of words"""""""
-verb = input('verb: ').upper()
-noun = input('noun: ').upper()
-abverb = input('abverb: ')
-exclamation = input('exclamation: ').capitalize()
-adjective = input('adjective: ').upper()
-animal = input('animal: ').upper()
-print(" ")
-# My story line""" 
-print() 
-print(' My story line: ')
-print()
-print(f"The other day, after returning from church. I decided to {verb} my {noun} and prepare for my next week course work, i head my siblings crying and some were very {adjective} and i asked, what happen, they said that my {animal} gave birth, i was like {exclamation}!. Out of excitement i give my friend one of the puppy.")
+
+verb1 = input('Please enter a verb: ').upper()
+noun = input('Please enter a noun: ').upper()
+verb2 = input('Please enter a verb: ')
+exclamation = input('Please enter an exclamation: ').capitalize()
+adjective = input('Please enter an adjective: ').upper()
+animal = input('Please enter an animal: ').upper()
+
+# My story line"""
+
+words = f'''The other day, after returning from church. 
+ \n I decided to {verb1} my {noun} and prepare for my next week course work.
+ \nI head my siblings crying and some were very {adjective} and i asked.
+ \nWhat happen, they said that my {animal} gave birth, i was like 
+ \n{exclamation}.
+ \nOut of excitement i {verb2} my friend one of the puppy.'''
+
+print(words)
